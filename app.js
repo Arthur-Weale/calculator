dec78a5a7b0bc620c396c93799b7201deaafb69f
Arthur Weale <ajayrossy340@gmail.com>
2025-03-01 18:47:11 +0200

    Did not follow instructions to refactor code

---
+++ app.js
@@ -0,0 +1,12 @@
+const first = document.querySelector(".first");
+const second = document.querySelector(".second");
+const result = document.querySelector(".result");
+const plus = document.querySelector(".add");
+const minus = document.querySelector(".sub");
+const times = document.querySelector(".mult");
+const divided = document.querySelector(".div");
+const equals = document.querySelector(".equal")
+
+const operate = function (){
+    
+}
